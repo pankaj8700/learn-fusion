486ce01856ff68281198b94675fc15e69e85a2d2
WAD <pankajku.8700@gmail.com>
2024-06-13 12:43:53 +0530

    complete 1st project

---
+++ project_1.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be positive integer"
+    elif n == 1:
+        return [0]
+    else:
+        fib_sequence = [0, 1]
+        for _ in range(n - 2):
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
+print(fibonacci(10))
